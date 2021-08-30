5f4c5246bd5c1b0e338265a4b135550708be964b
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-08-30 18:12:52 -0300

    Add a functional cancel button to mobile menu

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let hambIcon = document.querySelector("#hamb-icon");
 let mobileMenu = document.querySelector(".mobile-menu");
+let cancelButton = document.querySelector("#cancel-button")
 
 hambIcon.addEventListener("click", function () {
   mobileMenu.classList.remove("d-none");
+});
+
+cancelButton.addEventListener("click", function () {
+  mobileMenu.classList.add("d-none");
 });
