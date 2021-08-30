35cc4e8fd7e0a667d137f88835970fc4759e45d2
Dico Diaz <dicodiaz@gmail.com>
2021-08-30 16:54:21 -0500

    Add menu items and fixed JS linter errors
    * Whenever you click a menu item, the menu closes.

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
-let hambIcon = document.querySelector("#hamb-icon");
-let mobileMenu = document.querySelector(".mobile-menu");
-let cancelButton = document.querySelector("#cancel-button")
+const hambIcon = document.querySelector('#hamb-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cancelButton = document.querySelector('#cancel-button');
+const menuItems = document.querySelectorAll('.mobile-menu-list > li');
 
-hambIcon.addEventListener("click", function () {
-  mobileMenu.classList.remove("d-none");
+hambIcon.addEventListener('click', () => {
+  mobileMenu.classList.remove('d-none');
 });
 
-cancelButton.addEventListener("click", function () {
-  mobileMenu.classList.add("d-none");
+cancelButton.addEventListener('click', () => {
+  mobileMenu.classList.add('d-none');
+});
+
+menuItems[0].addEventListener('click', () => {
+  mobileMenu.classList.add('d-none');
+});
+
+menuItems[1].addEventListener('click', () => {
+  mobileMenu.classList.add('d-none');
+});
+
+menuItems[2].addEventListener('click', () => {
+  mobileMenu.classList.add('d-none');
 });
