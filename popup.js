c7c99c306ed2c6700943961a40516acd7e0d6aab
Dico Diaz <dicodiaz@gmail.com>
2021-08-31 12:45:01 -0500

    Add popup section
    * Add popup html section with empty template
    * Add popup.js file
    * Hide the section with css and show it with js when you press any button
    * Create 6 project object with their own properties
    * Create projects array with each project object as an element
    * Fill the empty template dinamycally using querySelector and it's properties

---
+++ popup.js
@@ -0,0 +1,81 @@
+const popup = document.querySelector('.popup');
+const projectButtons = document.querySelectorAll('.project-button');
+
+const project1 = {
+  name: 'Multi Post Stories',
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'img/project-image.svg',
+  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+  live: 'http://dicodiaz.me/google-homepage/',
+  source: 'https://github.com/dicodiaz/google-homepage',
+};
+const project2 = {
+  name: 'Multi Post Stories',
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'img/project-image.svg',
+  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+  live: 'http://dicodiaz.me/google-homepage/',
+  source: 'https://github.com/dicodiaz/google-homepage',
+};
+const project3 = {
+  name: 'sakdjfhajksdf',
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'img/project-image.svg',
+  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+  live: 'http://dicodiaz.me/google-homepage/',
+  source: 'https://github.com/dicodiaz/google-homepage',
+};
+const project4 = {
+  name: 'Multi Post Stories',
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'img/project-image.svg',
+  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+  live: 'http://dicodiaz.me/google-homepage/',
+  source: 'https://github.com/dicodiaz/google-homepage',
+};
+const project5 = {
+  name: 'Multi Post Stories',
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'img/project-image.svg',
+  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+  live: 'http://dicodiaz.me/google-homepage/',
+  source: 'https://github.com/dicodiaz/google-homepage',
+};
+const project6 = {
+  name: 'Multi Post Stories',
+  description:
+    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent",
+  image: 'img/project-image.svg',
+  technologies: ['HTML', 'Bootstrap', 'Ruby on Rails'],
+  live: 'http://dicodiaz.me/google-homepage/',
+  source: 'https://github.com/dicodiaz/google-homepage',
+};
+
+const projects = [project1, project2, project3, project4, project5, project6];
+
+projectButtons.forEach((item, i) => {
+  const popupTitle = document.querySelector('.popup-title');
+  const popupTech = document.querySelector('.popup-tech');
+  const popupImg = document.querySelector('.popup-img');
+  const popupDesc = document.querySelector('.popup-desc');
+  const popupLive = document.querySelector('.btn-live');
+  const popupSource = document.querySelector('.btn-source');
+
+  item.addEventListener('click', () => {
+    popup.classList.remove('d-none');
+    popupTitle.innerHTML = projects[i - 1].name;
+    projects[i - 1].technologies.forEach((tech) => {
+      const li = document.createElement('li');
+      li.className = 'popup-tech-item';
+      li.innerHTML = tech;
+      popupTech.appendChild(li);
+    });
+    popupImg.src = projects[i - 1].image;
+    popupDesc.innerHTML = projects[i - 1].description;
+  });
+});
