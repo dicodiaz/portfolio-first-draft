666b20cb8f2189e36003f62cf7335edde7721b84
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-09-01 17:24:50 -0300

    Add Error Message for email validation

--- form-validation.js
+++ form-validation.js
@@ -1,5 +1,8 @@
 const form = document.querySelector('#contact-form');
 const email = form.elements['user-email'];
+const msg = document.querySelector('#error-msg');
+const ERROR_MESSAGE = 'The email address has to be in lowercase.';
+const emailContainer = document.querySelector('#email-container');
 
 function isLowerCase(str) {
   return str === str.toLowerCase();
@@ -7,9 +10,11 @@ function isLowerCase(str) {
 
 form.addEventListener('submit', (event) => {
   if (!isLowerCase(email.value)) {
-    alert('Email is not lowercase');
+    msg.innerText = ERROR_MESSAGE;
+    emailContainer.classList.add('email-input-error');
     event.preventDefault();
   } else {
-    alert('Email is lowercase');
+    msg.innerText = '';
+    emailContainer.classList.remove('email-input-error');
   }
 });
